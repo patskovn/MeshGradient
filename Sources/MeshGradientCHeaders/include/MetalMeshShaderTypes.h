e800432970b23148c0375fb4b8bcce479364fe11
Frederik Riedel <frederik.riedel@frogg.io>
2024-07-20 22:41:45 +0200

    adds import

--- Sources/MeshGradientCHeaders/include/MetalMeshShaderTypes.h
+++ Sources/MeshGradientCHeaders/include/MetalMeshShaderTypes.h
@@ -8,6 +8,8 @@
 #ifndef MetalMeshShaderTypes_h
 #define MetalMeshShaderTypes_h
 
+import Darwin.POSIX.sys.types
+
 #include <simd/simd.h>
 // Buffer index values shared between shader and C code to ensure Metal shader buffer inputs
 // match Metal API buffer set calls.
